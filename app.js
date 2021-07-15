2503b43b3fcf284a20d4579725880b885fd0fa6d
Stewart <stewart@mail.com>
2021-07-15 15:18:33 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+import getUser from './utils/fetch-User.js';
+import displayUser from './utils/displayUser.js';
+import get from './utils/getElement.js';
+
+const btn = get('.btn');
+
+
+
+
+const showUser = async () => {
+    // get user from api 
+    const person = await getUser();
+    displayUser(person);
+    //display user
+};
+
+window.addEventListener('DOMContentLoaded', showUser);
+btn.addEventListener('click', showUser);
+
+
+
+
